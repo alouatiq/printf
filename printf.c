7e54d1d7398bf254680a38140a1ad6da27702ed2
Adewole Gege <100136717+Adewole-Gege@users.noreply.github.com>
2024-07-31 18:51:43 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,97 +1,93 @@
 #include "main.h"
-#include <stdlib.h>
-
+#include <stdarg.h>
+#include <unistd.h>
 /**
- * _printf - Custom implementation of printf
- * @format: Format string
-  * Return: Number of characters printed
- */
+* _printf - produces output according to a format
+* @format: format string containing the characters and the specifiers
+* Description: this function will call the corresponding function
+* that will handle the format
+* Return: length of the formatted output string
+*/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *ptr;
-    char *str;
-    char c;
-    int d;
-    char buffer[20]; // Buffer for integer to string conversion
-
-    va_start(args, format);
-    for (ptr = format; *ptr != '\0'; ptr++)
-    {
-        if (*ptr == '%')
-        {
-            ptr++;
-            if (*ptr == 'c')
-            {
-                c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            }
-            else if (*ptr == 's')
-            {
-                str = va_arg(args, char *);
-                while (*str)
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*ptr == 'd' || *ptr == 'i')
-            {
-                d = va_arg(args, int);
-                itoa(d, buffer, 10);
-                str = buffer;
-                while (*str)
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*ptr == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-        }
-        else
-        {
-            write(1, ptr, 1);
-            count++;
-        }
-    }
-    va_end(args);
-    return count;
-}
-
-/**
- * itoa - Convert integer to string
- * @value: Integer value
- * @str: Buffer to store the string
- * @base: Numerical base
- * Return: Pointer to the buffer
- */
-char *itoa(int value, char *str, int base)
+int count = 0;
+va_list args;
+int i = 0;
+va_start(args, format);
+if (!format)
+return (-1);
+while (format && format[i])
 {
-    char *ptr = str, *ptr1 = str, tmp_char;
-    int tmp_value;
-
-    if (base < 2 || base > 36) { *str = '\0'; return str; }
-
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "0123456789abcdefghijklmnopqrstuvwxyz"[tmp_value - value * base];
-    } while ( value );
-
-    if (tmp_value < 0) *ptr++ = '-';
-    *ptr-- = '\0';
-    while (ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr-- = *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return str;
+if (format[i] == '%')
+{
+i++;
+if (format[i] == 'c')
+count += _putchar(va_arg(args, int));
+else if (format[i] == 's')
+count += _puts(va_arg(args, char *));
+else if (format[i] == '%')
+count += _putchar('%');
+else if (format[i] == 'd' || format[i] == 'i')
+count += _print_number(va_arg(args, int));
+else
+return (-1);
+}
+else
+count += _putchar(format[i]);
+i++;
+}
+va_end(args);
+return (count);
+}
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+/**
+* _puts - prints a string to stdout
+* @str: the string to print
+*
+* Return: number of characters printed
+*/
+int _puts(char *str)
+{
+int i = 0;
+if (!str)
+str = "(null)";
+while (str[i])
+_putchar(str[i++]);
+return (i);
+}
+/**
+* _print_number - prints an integer to stdout
+* @n: the integer to print
+*
+* Return: number of characters printed
+*/
+int _print_number(int n)
+{
+unsigned int num;
+int count = 0;
+if (n < 0)
+{
+_putchar('-');
+count++;
+num = -n;
+}
+else
+{
+num = n;
+}
+if (num / 10)
+count += _print_number(num / 10);
+_putchar((num % 10) + '0');
+count++;
+return (count);
 }
