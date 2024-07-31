c160a8a51648efa33042d21914e2f1a3f91716f3
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-07-31 18:21:20 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,48 +8,44 @@
 */
 int _printf(const char *format, ...)
 {
-int count = 0;
-const char *p;
-va_list args;
-char c;
-char *s;
-va_start(args, format);
-for (p = format; *p != '\0'; p++)
-{
-if (*p == '%')
-{
-p++;
-switch (*p)
-{
-case 'c':
-c = va_arg(args, int);
-write(1, &c, 1);
-count++;
-break;
-case 's':
-s = va_arg(args, char *);
-while (*s)
-{
-write(1, s++, 1);
-count++;
-}
-break;
-case '%':
-write(1, "%", 1);
-count++;
-break;
-default:
-write(1, p, 1);
-count++;
-break;
-}
-}
-else
-{
-write(1, p, 1);
-count++;
-}
-}
-va_end(args);
-return count;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
+
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
