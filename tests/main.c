b8f62bfa4ed4180ca2704a4c79ead00e00ba327c
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-07-31 19:08:07 +0200

    Update main.c

--- tests/main.c
+++ tests/main.c
@@ -3,13 +3,14 @@
 
 /**
  * main - Entry point
+ * 
  * Return: Always 0
  */
 int main(void)
 {
-  _printf("Character:[%c]\n", 'H');
-  _printf("String:[%s]\n", "I am a string !");
-  _printf("Percent:[%%]\n");
+    _printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    _printf("Percent:[%%]\n");
 
-  return (0);
+    return (0);
 }
