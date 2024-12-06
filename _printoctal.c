b39ca1e3c2c61bc744319f334dc8c12b8bbc37ad
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 08:03:42 +0100

    Update _printoctal.c

--- _printoctal.c
+++ _printoctal.c
@@ -1,100 +1,33 @@
 #include "main.h"
 
 /**
- * _printreverse - Prints a string in reverse
- * @args: The argument list containing the string to print
+ * _printoctal - Prints an unsigned integer in octal format
+ * @args: The argument list containing the unsigned integer to print
  *
  * Return: The number of characters printed
  */
-int _printreverse(va_list args)
+int _printoctal(va_list args)
 {
-    char *str = va_arg(args, char *);
-    int i, count = 0;
+    unsigned int num = va_arg(args, unsigned int);
+    char oct[32];
+    int i = 0, count = 0;
 
-    if (!str)
-        str = "(null)";
-
-    for (i = 0; str[i]; i++) /* Find string length */
-        ;
-
-    for (i -= 1; i >= 0; i--) /* Print string in reverse */
+    if (num == 0)
     {
-        _putchar(str[i]);
-        count++;
+        _putchar('0');
+        return (1);
     }
 
-    return (count);
-}
-
-/**
- * _printrot13 - Encodes a string using ROT13 and prints it
- * @args: The argument list containing the string to encode and print
- *
- * Return: The number of characters printed
- */
-int _printrot13(va_list args)
-{
-    char *str = va_arg(args, char *);
-    int i, count = 0;
-    char c;
-
-    if (!str)
-        str = "(null)";
-
-    for (i = 0; str[i]; i++)
+    while (num > 0)
     {
-        c = str[i];
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-        {
-            if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
-                c += 13;
-            else
-                c -= 13;
-        }
-        _putchar(c);
-        count++;
+        oct[i++] = (num % 8) + '0'; /* Convert to octal digit */
+        num /= 8;
     }
 
-    return (count);
-}
-
-/**
- * _printnonprintable - Prints a string with non-printable characters as hex codes
- * @args: The argument list containing the string to print
- *
- * Return: The number of characters printed
- */
-int _printnonprintable(va_list args)
-{
-    char *str = va_arg(args, char *);
-    int i, count = 0;
-
-    if (!str)
-        str = "(null)";
-
-    for (i = 0; str[i]; i++)
+    while (--i >= 0) /* Print the octal digits in reverse order */
     {
-        if (str[i] < 32 || str[i] >= 127) /* Non-printable characters */
-        {
-            _putchar('\\');
-            _putchar('x');
-            count += 2;
-
-            if (str[i] < 16) /* Add leading zero for single-digit hex */
-            {
-                _putchar('0');
-                count++;
-            }
-
-            count += _putchar((str[i] / 16) + '0'); /* First hex digit */
-            count += _putchar((str[i] % 16) < 10 ? (str[i] % 16) + '0'
-                                                 : (str[i] % 16) - 10 + 'A'); /* Second hex digit */
-        }
-        else
-        {
-            _putchar(str[i]);
-            count++;
-        }
+        _putchar(oct[i]);
+        count++;
     }
 
     return (count);
