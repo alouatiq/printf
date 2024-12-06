cb4da300006b1f0b5bc927f7d019756ae9221e86
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 08:20:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,7 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
+    int len, len2;
     unsigned int ui;
     void *addr;
 
@@ -18,6 +17,7 @@ int main(void)
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
+
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -36,11 +36,10 @@ int main(void)
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]
-", len);
-    printf("Len:[%d]
-", len2);
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    printf("Unknown:[%%r]\n");
+
     return (0);
 }
