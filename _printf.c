05b87f911abb07a8ccabdfd2b551c690b29a28cb
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 08:40:41 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -13,15 +13,15 @@
 int _printf(const char *format, ...)
 {
     va_list args;
-    int i = 0, count = 0, buffer_index = 0;
     char buffer[BUFFER_SIZE];
+    int buffer_index = 0, i = 0, count = 0;
 
     if (!format)
         return (-1);
 
     va_start(args, format);
 
-    while (format && format[i])
+    while (format[i])
     {
         if (format[i] == '%')
         {
@@ -32,52 +32,25 @@ int _printf(const char *format, ...)
                 return (-1);
             }
 
-            /* Old cases preserved */
-            if (format[i] == 'c')
-                count += _add_to_buffer(buffer, &buffer_index, va_arg(args, int)); /* %c */
-            else if (format[i] == 's')
-                count += _printstring_to_buffer(buffer, &buffer_index, va_arg(args, char *)); /* %s */
-            else if (format[i] == 'd' || format[i] == 'i')
-                count += _printint_to_buffer(buffer, &buffer_index, va_arg(args, int)); /* %d, %i */
-            else if (format[i] == '%')
-                count += _add_to_buffer(buffer, &buffer_index, '%'); /* %% */
-            else
+            switch (format[i])
             {
-                /* New cases for advanced specifiers */
-                switch (format[i])
-                {
-                case 'b':
-                    count += _printbinary_to_buffer(buffer, &buffer_index, va_arg(args, unsigned int));
+                case 'c':
+                    count += _add_to_buffer(buffer, &buffer_index, va_arg(args, int));
                     break;
-                case 'u':
-                    count += _printunsigned_to_buffer(buffer, &buffer_index, va_arg(args, unsigned int));
+                case 's':
+                    count += _printstring_to_buffer(buffer, &buffer_index, va_arg(args, char *));
                     break;
-                case 'o':
-                    count += _printoctal_to_buffer(buffer, &buffer_index, va_arg(args, unsigned int));
+                case 'd':
+                case 'i':
+                    count += _printint_to_buffer(buffer, &buffer_index, va_arg(args, int));
                     break;
-                case 'x':
-                    count += _printhex_to_buffer(buffer, &buffer_index, va_arg(args, unsigned int), 0);
-                    break;
-                case 'X':
-                    count += _printhex_to_buffer(buffer, &buffer_index, va_arg(args, unsigned int), 1);
-                    break;
-                case 'p':
-                    count += _printpointer_to_buffer(buffer, &buffer_index, va_arg(args, void *));
-                    break;
-                case 'r':
-                    count += _printreverse_to_buffer(buffer, &buffer_index, va_arg(args, char *));
-                    break;
-                case 'R':
-                    count += _printrot13_to_buffer(buffer, &buffer_index, va_arg(args, char *));
-                    break;
-                case 'S':
-                    count += _printnonprintable_to_buffer(buffer, &buffer_index, va_arg(args, char *));
+                case '%':
+                    count += _add_to_buffer(buffer, &buffer_index, '%');
                     break;
                 default:
                     count += _add_to_buffer(buffer, &buffer_index, '%');
                     count += _add_to_buffer(buffer, &buffer_index, format[i]);
                     break;
-                }
             }
         }
         else
@@ -87,7 +60,7 @@ int _printf(const char *format, ...)
         i++;
     }
 
-    /* Flush remaining characters in the buffer */
+    /* Flush any remaining characters in the buffer */
     if (buffer_index > 0)
         count += _write_buffer(buffer, buffer_index);
 
