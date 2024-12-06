14f377e4ff5315723608520b63deeca808310943
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 08:02:31 +0100

    Create _printpointer.c

---
+++ _printpointer.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _printpointer - Prints a memory address in hexadecimal format
+ * @args: The argument list containing the pointer to print
+ *
+ * Return: The number of characters printed
+ */
+int _printpointer(va_list args)
+{
+    void *ptr = va_arg(args, void *);
+    unsigned long int address;
+    char hex[16];
+    int i = 0, count = 0;
+
+    if (!ptr)
+    {
+        return (_printstring("(nil)")); /* Print "(nil)" for NULL pointers */
+    }
+
+    address = (unsigned long int)ptr;
+
+    /* Convert address to hexadecimal */
+    while (address > 0)
+    {
+        unsigned long int num = address % 16;
+        hex[i++] = (num < 10) ? (num + '0') : ((num - 10) + 'a');
+        address /= 16;
+    }
+
+    /* Print "0x" prefix for pointers */
+    _putchar('0');
+    _putchar('x');
+    count += 2;
+
+    /* Print hexadecimal address in reverse order */
+    while (--i >= 0)
+    {
+        _putchar(hex[i]);
+        count++;
+    }
+
+    return (count);
+}
