7bf1e3e66e7dfce089f18e61f5393c77dc7e62a5
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 08:25:54 +0100

    Update main.h

--- main.h
+++ main.h
@@ -5,14 +5,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/* Main function */
+/* Main printf function */
 int _printf(const char *format, ...); /* Main printf function */
 
-/* Buffer management */
+/* Low-Level Output Functions */
+int _putchar(char c);                /* Writes a single character to stdout */
+
+/* Buffer Management Functions */
 int _add_to_buffer(char *buffer, int *index, char c); /* Add character to buffer */
 int _write_buffer(char *buffer, int len);            /* Flush buffer to stdout */
 
-/* Handlers for specifiers */
+/* Handlers for Specifiers */
 int _printchar(va_list args);        /* Handles %c */
 int _printstring(va_list args);      /* Handles %s */
 int _printint(va_list args);         /* Handles %d and %i */
@@ -25,7 +28,7 @@ int _printreverse(va_list args);     /* Handles %r */
 int _printrot13(va_list args);       /* Handles %R */
 int _printnonprintable(va_list args); /* Handles %S */
 
-/* Utility functions */
+/* Utility Functions */
 int _strlen(char *str);              /* String length utility */
 char *_itoa(int num, char *str, int base); /* Integer to string conversion */
 char *_reverse(char *str);           /* Reverse a string */
