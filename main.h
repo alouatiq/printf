50052c5c74150f089dbee2d97eaa486ec1e68594
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 07:57:56 +0100

    Update main.h

--- main.h
+++ main.h
@@ -5,10 +5,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int _printchar(va_list args);
-int _printstring(va_list args);
-int _printint(va_list args);
+/* Function prototypes */
+int _printf(const char *format, ...); /* Main printf function */
+int _putchar(char c);                /* Writes a character to stdout */
+int _printchar(va_list args);        /* Handles %c */
+int _printstring(va_list args);      /* Handles %s */
+int _printint(va_list args);         /* Handles %d and %i */
 
 #endif /* MAIN_H */
