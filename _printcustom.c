6bd5bcc69fa3d7ae5b3ea25a7357db46e5f70f07
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-06 08:02:57 +0100

    Create _printcustom.c

---
+++ _printcustom.c
@@ -0,0 +1,89 @@
+#include "main.h"
+
+/**
+ * _printreverse - Prints a string in reverse
+ * @args: The argument list containing the string to print
+ *
+ * Return: The number of characters printed
+ */
+int _printreverse(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int i, count = 0;
+
+    if (!str)
+        str = "(null)";
+
+    for (i = 0; str[i]; i++) /* Find string length */
+        ;
+
+    for (i -= 1; i >= 0; i--) /* Print string in reverse */
+    {
+        _putchar(str[i]);
+        count++;
+    }
+
+    return (count);
+}
+
+/**
+ * _printrot13 - Encodes a string using ROT13 and prints it
+ * @args: The argument list containing the string to encode and print
+ *
+ * Return: The number of characters printed
+ */
+int _printrot13(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int i, count = 0;
+    char c;
+
+    if (!str)
+        str = "(null)";
+
+    for (i = 0; str[i]; i++)
+    {
+        c = str[i];
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        {
+            if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
+                c += 13;
+            else
+                c -= 13;
+        }
+        _putchar(c);
+        count++;
+    }
+
+    return (count);
+}
+
+/**
+ * _printnonprintable - Prints a string with non-printable characters as hex codes
+ * @args: The argument list containing the string to print
+ *
+ * Return: The number of characters printed
+ */
+int _printnonprintable(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int i, count = 0;
+
+    if (!str)
+        str = "(null)";
+
+    for (i = 0; str[i]; i++)
+    {
+        if (str[i] < 32 || str[i] >= 127) /* Non-printable characters */
+        {
+            _putchar('\\');
+            _putchar('x');
+            count += 2;
+
+            if (str[i] < 16) /* Add leading zero for single-digit hex */
+            {
+                _putchar('0');
+                count++;
+            }
+
+      
